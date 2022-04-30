dd534f4c30ba4ca41f4d04217b7fd933ecc5e701
RizkiAshari <rizkiashari1026@gmail.com>
2022-04-30 16:03:04 +0700

    Navigation Active

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,19 +1,33 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Navigation: React.FC = () => {
+  const { pathname } = useRouter();
+
+  console.log(pathname);
+
   return (
     <nav className='flex justify-between items-center px-4 py-2 container flex-wrap'>
       <Link passHref href='/'>
         Rizki Ashari
       </Link>
       <ul className='flex items-center'>
-        <li className='px-2 mx-2 text-gray-200 text-sm'>
+        <li
+          className={`px-2 mx-2 text-sm ${
+            pathname === "/" ? "text-primary-300" : "text-gray-200"
+          }`}>
           <Link href='/'>Home</Link>
         </li>
-        <li className='px-2 mx-2 text-gray-200 text-sm'>
+        <li
+          className={`px-2 mx-2 text-sm ${
+            pathname === "/about-me" ? "text-primary-300" : "text-gray-200"
+          }`}>
           <Link href='/about-me'>About me</Link>
         </li>
-        <li className='px-2 mx-2 text-gray-200 text-sm'>
+        <li
+          className={`px-2 mx-2 text-sm ${
+            pathname === "/portfolio" ? "text-primary-300" : "text-gray-200"
+          }`}>
           <Link href='/portfolio'>Portfolio</Link>
         </li>
         <li className='btn-primary'>
